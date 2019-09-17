13bc944c043bdba13c36d3da458bb948e6424b97
spemed <945451501@qq.com>
2019-09-17 14:04:16 +0800

    修正数据库助手函数错误信息,定义错误返回,新增配置项管理

---
+++ util/redis/connection.go
@@ -0,0 +1,14 @@
+package redis
+
+import (
+	"github.com/go-redis/redis"
+)
+
+const (
+	USERNAME = "root"
+	PASSWORD = "root"
+	NETWORK  = "tcp"
+	SERVER   = "localhost"
+	PORT     =  6379
+	DATABASE = "chatroom"
+)
